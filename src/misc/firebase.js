dea74a3a2c17d30414104d05ed3e1bd975295597
Kunal <kunalsharma.280104@gmail.com>
2022-08-23 13:33:46 +0530

    Initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyB7KBA957BniYaCAEcrrTkQf25I7b7auRE',
+  authDomain: 'chat-web-app-b72f4.firebaseapp.com',
+  projectId: 'chat-web-app-b72f4',
+  storageBucket: 'chat-web-app-b72f4.appspot.com',
+  messagingSenderId: '1010674998140',
+  appId: '1:1010674998140:web:490e76b7286f8d6915a2f9',
+  databaseURL:
+    'https://chat-web-app-b72f4-default-rtdb.asia-southeast1.firebasedatabase.app/',
+};
+
+const app = firebase.initializeApp(config);
