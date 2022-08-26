aca1796dcd4456aa37bc86bb064ce4a856fe7803
Kunal <kunalsharma.280104@gmail.com>
2022-08-26 17:52:29 +0530

    Started to work with chat room list

---
+++ src/components/rooms/ChatRoomList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Nav } from 'rsuite';
+import RoomItem from './RoomItem';
+
+const ChatRoomList = ({ AboveElHeight }) => {
+  return (
+    <Nav
+      appearance="subtle"
+      vertical
+      reversed
+      className="overflow-y-scroll custom-scroll"
+      style={{
+        height: `calc(100% - ${AboveElHeight}px)`,
+      }}
+    >
+      <Nav.Item>
+        <RoomItem />
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default ChatRoomList;
