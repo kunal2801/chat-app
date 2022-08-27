0ee216dacf0ee285d9724eb0ba6cd6013a77ebd9
Kunal <kunalsharma.280104@gmail.com>
2022-08-27 14:14:07 +0530

    Update all user references if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -6,6 +6,7 @@ import { useModalState } from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdate } from '../../misc/helpers';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 
@@ -55,10 +56,14 @@ const AvatarUploadBtn = () => {
         cacheControl: `public, max-age${3600 * 24 * 3}`,
       });
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
-      userAvatarRef.set(downloadUrl);
+
+      const updates = await getUserUpdate(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
+      await database.ref().update(updates);
       setIsLoading(false);
       Alert.info('Avatar has been uploaded', 4000);
     } catch (err) {
